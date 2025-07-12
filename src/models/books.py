086833daa0f22318cb2bc70947d7a434128012d2
IllyaDD <illya.d.donchenko@gmail.com>
2025-07-12 11:27:47 +0300

    Написав пару файлів

--- src/models/books.py
+++ src/models/books.py
@@ -1,17 +1,16 @@
 from datetime import datetime
 from typing import Optional
 
-from sqlalchemy import Column, DateTime, Integer, String, VARCHAR
+from sqlalchemy import Column, DateTime, VARCHAR, Integer
 from sqlmodel import Field, SQLModel, Relationship      
-
 
 class Book(SQLModel, table=True):
     __tablename__ = "books"
     
     id: Optional[int] = Field(primary_key=True)
-    name:str = Field(sa_value=Column(VARCHAR(100), nullable=False))
-    description: str = Field(sa_value=Column(VARCHAR(500), nullable=True))
-    link: str = Field(sa_value=Column(VARCHAR(500), nullable=True))
+    name: str = Field(sa_column=Column(VARCHAR(100), nullable=False))
+    description: Optional[str] = Field(sa_column=Column(VARCHAR(500), nullable=True))
+    link: Optional[str] = Field(sa_column=Column(VARCHAR(500), nullable=True))
     created_at: Optional[datetime] = Field(sa_column=Column(DateTime(timezone=True), nullable=False))
-    
-    
+    shelf_id: int = Field(foreign_key="shelves.id", nullable=False)
+    shelf: "Shelf" = Relationship(back_populates="books")
--- src/models/shelfs.py
+++ src/models/shelfs.py
@@ -8,9 +8,7 @@ class Shelf(SQLModel, table=True):
     __tablename__ = "shelves"
 
     id: Optional[int] = Field(primary_key=True)
-    name: str = Field(sa_value=Column(VARCHAR(100), nullable=False))
-    description: Optional[str] = Field(sa_value=Column(VARCHAR(500), nullable=True))
+    name: str = Field(sa_column=Column(VARCHAR(100), nullable=False))
+    description: Optional[str] = Field(sa_column=Column(VARCHAR(500), nullable=True))
     created_at: Optional[datetime] = Field(sa_column=Column(DateTime(timezone=True), nullable=False))
-    
-    
-    books: List["Book"] = Relationship(back_populates="shelf")  # Assuming Book model has a shelf relationship
+    books: List["Book"] = Relationship(back_populates="shelf")
